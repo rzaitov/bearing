9252b77efc91d024e7552e203465fac74439a052
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-07-05 13:27:09 +0300

    read headers from xls

---
+++ BearingTools/BearingTools/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+using ExcelLibrary.SpreadSheet;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SchemaGenerator
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			/*
+			Workbook book = Workbook.Load ("Dictionary.xls");
+			Worksheet ws = book.Worksheets.Where (w => w.Name == "URL").Single ();
+
+			CellCollection cells = ws.Cells;
+
+			int column = -1;
+			while (true) {
+				var cell = cells [0, ++column];
+				if (cell.StringValue == "Путь к файлу")
+					break;
+			}
+
+			var notFound = new List<string> ();
+			int row = 0;
+			int cnt = 0;
+			while (true) {
+				var cell = cells [++row, column];
+
+				if (cell.Value == null)
+					break;
+
+				string path = cell.StringValue;
+				if (!path.StartsWith ("skf"))
+					continue;
+
+				path = path.Replace ("\\", "/");
+				path = Path.Combine ("/Users/rzaitov/Documents/Apps/_Bearing/", path);
+
+				if (!File.Exists (path))
+					notFound.Add (path);
+				else
+					cnt++;
+			}
+
+			int i = 0;
+			Console.WriteLine ("Not founded:");
+			foreach (var p in notFound)
+				Console.WriteLine ("{0} {1}", ++i, p);
+
+			Console.WriteLine ("Scanned rows: {0}. Found {1} files", row - 1, cnt);
+			*/
+
+			var tr = new TableReader ();
+			tr.Read ("/Users/rzaitov/Documents/Apps/_Bearing/skf/Подшипники/Шариковые/Шариковые Упорные/Упорные шарикоподшипники, одинарные.xls");
+		}
+	}
+}
---
+++ BearingTools/BearingTools/TableReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NPOI.SS.UserModel;
+
+namespace SchemaGenerator
+{
+	public class TableReader
+	{
+		readonly List<string> files;
+		readonly List<string> notFound;
+		readonly HashSet<string> headers;
+
+		public TableReader ()
+		{
+			files = new List<string> ();
+			notFound = new List<string> ();
+			headers = new HashSet<string> ();
+		}
+
+		public void Read (string path)
+		{
+			if (!File.Exists (path)) {
+				notFound.Add (path);
+				return;
+			}
+
+			IWorkbook wb = WorkbookFactory.Create (path);
+			ISheet sheet = wb.GetSheetAt (0);
+
+			int column = 0;
+			while (true) {
+				ICell cell = sheet.GetRow (0).GetCell (column++);
+
+				if (cell == null)
+					break;
+
+				headers.Add (cell.StringCellValue);
+			}
+
+			foreach (var h in headers) {
+				Console.WriteLine (h);
+			}
+		}
+	}
+}
+
